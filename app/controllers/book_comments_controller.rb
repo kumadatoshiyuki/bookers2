634afbc33a8c56c138f22bb7c365e05a0cf033d4
EC2 Default User <ec2-user@ip-172-31-66-186.ec2.internal>
2020-11-03 09:02:17 +0000

    [Add]コメントバリデーション

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,11 +1,14 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    # binding.pry
+    @book_show = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book_show.id
+  if @comment.save
     redirect_to book_path(book)
+  else
+    @book_comment = BookComment.new()
+    render("books/show")
+  end
   end
 
   def destroy
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,8 @@ class BooksController < ApplicationController
     @book_new = Book.new()
     @book_show = Book.find(params[:id])
     @user = User.find(current_user.id)
-    @book_comment = BookComment.new
+    @book_comment = BookComment.new()
+    
   end
 
   def new
